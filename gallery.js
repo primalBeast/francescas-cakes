bfc34abde16e89c02b83fe74f42226386934fbed
primalBeast <43726368+primalBeast@users.noreply.github.com>
2026-08-28 21:51:00 -0700

    Stop replacing JPEGs with missing .b64 fetches

--- gallery.js
+++ gallery.js
@@ -1,10 +1,1 @@
-document.querySelectorAll("img").forEach(function (img) {
-  var s = img.getAttribute("src") || "";
-  if (!/\.jpg$/i.test(s)) return;
-  fetch(s.replace(/\.jpg$/i, ".b64")).then(function (r) {
-    if (!r.ok) throw new Error(s);
-    return r.text();
-  }).then(function (t) {
-    img.src = "data:image/jpeg;base64," + t.trim();
-  }).catch(function () {});
-});
+/* cake stills load as JPEGs */
