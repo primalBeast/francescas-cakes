1ec065e641ea64b44f3073b1f637b758f3e03f78
primalBeast <43726368+primalBeast@users.noreply.github.com>
2026-08-28 21:48:12 -0700

    Load cake stills from sibling .b64 files

--- gallery.js
+++ gallery.js
@@ -1,10 +1,10 @@
-function loadB64Images(root) {
-  (root || document).querySelectorAll("img[data-b64]").forEach(function (img) {
-    if (img.dataset.loaded === "1") return;
-    img.dataset.loaded = "1";
-    fetch(img.getAttribute("data-b64")).then(function (r) { return r.text(); }).then(function (t) {
-      img.src = "data:image/jpeg;base64," + t.trim();
-    }).catch(function () {});
-  });
-}
-loadB64Images();
+document.querySelectorAll("img").forEach(function (img) {
+  var s = img.getAttribute("src") || "";
+  if (!/\.jpg$/i.test(s)) return;
+  fetch(s.replace(/\.jpg$/i, ".b64")).then(function (r) {
+    if (!r.ok) throw new Error(s);
+    return r.text();
+  }).then(function (t) {
+    img.src = "data:image/jpeg;base64," + t.trim();
+  }).catch(function () {});
+});
